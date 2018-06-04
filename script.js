c06be7ed19dc137c7548dcdeb56c1093a088420b
Cameron Samuels <me@cameronsamuels.com>
2018-06-04 11:18:05 -0500

    (feature)Added custom color in url

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.location.hash.length == 4 || window.location.hash.length == 7) document.body.style.background = window.location.hash;
+});
