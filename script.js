12e7f84c8e338d33b3053d23f821d9a576338680
Cameron Samuels <me@cameronsamuels.com>
2018-06-04 21:06:35 -0500

    (update)Updated the app

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-  if (window.location.hash.length == 4 || window.location.hash.length == 7) document.body.style.background = window.location.hash;
-});
+var url = window.location.toString();
+var text = url.substring(url.indexOf("?") + 1);
+var background = url.substring(url.indexOf("#")).replace("?" + text, "");
+
+if (url.includes("?"))
+  document.title = text,
+  document.documentElement.style.setProperty("--text", "'" + text + "'");
+
+if (background.length == 4 || background.length == 7)
+  document.documentElement.style.setProperty("--background", background);
