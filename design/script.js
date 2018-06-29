a3abeb7b9f224e9a55a040c96253589c77220595
Cameron Samuels <me@cameronsamuels.com>
2018-06-29 09:08:18 -0500

    (improvement)Updated the design page
    Added default placeholder text when blank ("Coming Soon!")

--- design/script.js
+++ design/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.querySelector("input[type='button']").addEventListener("click", function() {
-    var text = "?" + document.querySelector("input").value.replace(/ /g, "%20");
+    var text = "?" + (document.querySelector("input").value.replace(/ /g, "%20") || "Coming Soon!");
     var background = "#" + document.querySelectorAll("input")[1].value;
     var font = "&" + (document.querySelector(".jscolor").style.color == "rgb(0, 0, 0)" ? "black" : "white");
     window.open("https://cameronsamuels.com/placeholder/" + background + (text.length == 1 ? "" : text) + font);
